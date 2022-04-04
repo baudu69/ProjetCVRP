ab83faca02ddbf4c684a0faa7297e6fde02a8ea9
Alexis Giust <alx.giust@gmail.com>
2022-04-04 12:55:07 +0200

    Chemin implements List<Point>

--- src/main/java/fr/polytech/projet/model/Chemin.java
+++ src/main/java/fr/polytech/projet/model/Chemin.java
@@ -1,21 +1,157 @@
 package fr.polytech.projet.model;
 
-import java.util.ArrayList;
+import java.util.*;
 
-public class Chemin extends ArrayList<Point> {
+public class Chemin implements List<Point> {
 
-    public int distance() {
-        for (Point p : this) {
+	private final List<Point> points = new ArrayList<>();
+	private boolean changed = false;
 
-        }
+	/**
+	 * Renvoie la longueur totale du chemin
+	 */
+	public double longueur() {
+		double ret = 0;
 
-        return -1;
-    }
+		Point prev = null;
+		for (Point p : points) {
+			ret += p.distance(prev);
 
-    public int quantity() {
-        return this.stream()
-                .mapToInt(Point::q)
-                .sum();
-    }
+			prev = p;
+		}
 
+		return ret;
+	}
+
+	/**
+	 * Renvoie le poids total des colis transportés
+	 */
+	public int quantity() {
+		return points.stream()
+				.mapToInt(Point::q)
+				.sum();
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		
+		if (o == null || getClass() != o.getClass()) return false;
+		
+		Chemin that = (Chemin) o;
+		
+		return Objects.equals(points, that.points);
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(points);
+	}
+	
+	
+	
+	
+	@Override
+	public Point get(int index) {
+		return points.get(index);
+	}
+	@Override
+	public Point set(int index, Point element) {
+		changed = true;
+		return points.set(index, element);
+	}
+	@Override
+	public void add(int index, Point element) {
+		changed = true;
+		points.add(index, element);
+	}
+	@Override
+	public Point remove(int index) {
+		changed = true;
+		return points.remove(index);
+	}
+	@Override
+	public int indexOf(Object o) {
+		return points.indexOf(o);
+	}
+	@Override
+	public int lastIndexOf(Object o) {
+		return points.lastIndexOf(o);
+	}
+	@Override
+	public ListIterator<Point> listIterator() {
+		return points.listIterator();
+	}
+	@Override
+	public ListIterator<Point> listIterator(int index) {
+		return points.listIterator(index);
+	}
+	@Override
+	public List<Point> subList(int fromIndex, int toIndex) {
+		return points.subList(fromIndex, toIndex);
+	}
+
+	@Override
+	public int size() {
+		return points.size();
+	}
+	@Override
+	public boolean isEmpty() {
+		return points.isEmpty();
+	}
+	@Override
+	public boolean contains(Object o) {
+		return points.contains(o);
+	}
+	@Override
+	public Iterator<Point> iterator() {
+		return points.iterator();
+	}
+	@Override
+	public Object[] toArray() {
+		return points.toArray();
+	}
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return points.toArray(a);
+	}
+	@Override
+	public boolean add(Point point) {
+		changed = true;
+		return points.add(point);
+	}
+	@Override
+	public boolean remove(Object o) {
+		changed = true;
+		return points.remove(o);
+	}
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return points.containsAll(c);
+	}
+	@Override
+	public boolean addAll(Collection<? extends Point> c) {
+		changed = true;
+		return points.addAll(c);
+	}
+	@Override
+	public boolean addAll(int index, Collection<? extends Point> c) {
+		changed = true;
+		return points.addAll(index, c);
+	}
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		changed = true;
+		return points.removeAll(c);
+	}
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		changed = true;
+		return points.retainAll(c);
+	}
+	@Override
+	public void clear() {
+		changed = true;
+		points.clear();
+	}
 }
--- src/main/java/fr/polytech/projet/model/Point.java
+++ src/main/java/fr/polytech/projet/model/Point.java
@@ -3,10 +3,25 @@ package fr.polytech.projet.model;
 
 public record Point(int x, int y, int id, int q) {
 
-    /**
-     * @return true si depot
-     */
-    public boolean isDepot() {
-        return this.id == 0;
-    }
+	/**
+	 * @return {@code true} si depot
+	 */
+	public boolean isDepot() {
+		return this.id == 0;
+	}
+
+	/**
+	 * 
+	 * @param that
+	 * @return La distance avec le point passé en paramètre
+	 */
+	public double distance(final Point that) {
+		if (that == null) return 0;
+		
+		final int dx = x - that.x;
+		final int dy = y - that.y;
+		
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+
 }
