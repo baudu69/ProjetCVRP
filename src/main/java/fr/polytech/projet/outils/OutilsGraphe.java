4dd8978c7e03f2a002e05dc68a36146a4b943cc7
baudu <bastien.audu@gmail.com>
2022-04-04 12:23:03 +0200

    Generation aleatoire de chemin

--- src/main/java/fr/polytech/projet/outils/OutilsGraphe.java
+++ src/main/java/fr/polytech/projet/outils/OutilsGraphe.java
@@ -1,5 +1,29 @@
 package fr.polytech.projet.outils;
 
+import fr.polytech.projet.model.Chemin;
+
+import java.util.Random;
+
 public class OutilsGraphe {
+
+
+    /**
+     * Genere un nouveau chemin aleatoire
+     *
+     * @param chemin chemin entree
+     * @return nouveau chemin aleatoire
+     */
+    public static Chemin genererSolutionAleatoire(Chemin chemin) {
+        Chemin chemin1 = new Chemin();
+        chemin1.add(chemin.get(0));
+        chemin.remove(0);
+        Random random = new Random();
+        for (int i = 0; i < chemin.size(); i++) {
+            int index = random.nextInt(chemin.size());
+            chemin1.add(chemin.get(index));
+            chemin.remove(index);
+        }
+        return chemin1;
+    }
 
 }
