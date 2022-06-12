93cb2633db256fc06779decc7d836e88cb63752b
baudu <bastien.audu@gmail.com>
2022-06-12 15:23:27 +0200

    2

--- src/main/java/fr/polytech/projet/controller/PrompGraphController.java
+++ src/main/java/fr/polytech/projet/controller/PrompGraphController.java
@@ -1,8 +1,14 @@
 package fr.polytech.projet.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+
 import java.util.List;
 
 public class PrompGraphController {
+
+	@FXML
+	private LineChart lc_result;
 
 	public void setValue(List<Double> value) {
 
