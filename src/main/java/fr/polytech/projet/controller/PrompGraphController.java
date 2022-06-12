83cba6fd8f2a573fcd5da57f330300a7d8ee17e9
Bastien Audu <bastien.audu@gmail.com>
2022-06-12 17:28:13 +0200

    affichage optimum + pas tt les valeures si trop nombreuses

--- src/main/java/fr/polytech/projet/controller/PrompGraphController.java
+++ src/main/java/fr/polytech/projet/controller/PrompGraphController.java
@@ -3,21 +3,34 @@ package fr.polytech.projet.controller;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 
 import java.util.List;
 
 public class PrompGraphController {
 
 	@FXML
+	public Label lblOptimum;
+	@FXML
 	private LineChart<String, Double> lc_result;
 
 	public void setValue(List<Double> value) {
+		double valueMini = value.stream().mapToDouble(Double::doubleValue).min().orElseThrow();
+		int iteMini = value.indexOf(valueMini);
 		XYChart.Series<String, Double> series = new XYChart.Series<>();
 		series.setName("résultats");
 		for (int i = 0; i < value.size(); i++) {
 			Double v = value.get(i);
-			series.getData().add(new XYChart.Data<>(String.valueOf(i), v));
+			if (value.size() < 200) {
+				series.getData().add(new XYChart.Data<>(String.valueOf(i), v));
+			} else {
+				if (i % (value.size() / 100) == 0 || v == valueMini) {
+					series.getData().add(new XYChart.Data<>(String.valueOf(i), v));
+				}
+			}
+
 		}
 		lc_result.getData().add(series);
+		lblOptimum.setText("Optimum : " + iteMini + " itérations");
 	}
 }
